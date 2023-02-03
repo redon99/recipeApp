c1ee917c630607182dde9bd5495119f929276f6e
redon99 <redon.lutolli@gmail.com>
2023-02-03 01:28:07 +0100

    recipe controllers

--- backend/controllers/recipesController.js
+++ backend/controllers/recipesController.js
@@ -1,15 +1,30 @@
-exports.createRecipe = (req, res) => {
-  res.send('create recipe');
+const Recipe = require('./../models/recipeModel');
+const BadRequestError = require('./../errors/badRequest');
+const UnauthenticatedError = require('../errors/unauthenticated');
+
+exports.createRecipe = async (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    throw new BadRequestError('Please provide a title for recipe');
+  }
+  req.body.createdBy = req.user.userId;
+  const recipe = await Recipe.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      recipe,
+    },
+  });
 };
-exports.getAllRecipes = (req, res) => {
+exports.getAllRecipes = async (req, res) => {
   res.send('get all recipes');
 };
-exports.updateRecipe = (req, res) => {
+exports.updateRecipe = async (req, res) => {
   res.send('update recipe');
 };
-exports.deleteRecipe = (req, res) => {
+exports.deleteRecipe = async (req, res) => {
   res.send('delete recipe');
 };
-exports.showStats = (req, res) => {
+exports.showStats = async (req, res) => {
   res.send('show recipes stats');
 };
